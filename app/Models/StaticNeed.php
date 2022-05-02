187515ee6c719fc2edd357924e92fbc746756f0b
panad-laptop <christophevandeneede@hotmail.com>
2022-05-02 12:32:42 +0200

    changed date to return in ISO-8601

--- app/Models/StaticNeed.php
+++ app/Models/StaticNeed.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,5 +18,9 @@ class StaticNeed extends Model
     protected $hidden = [
         'created_at',
         'updated_at'
+    ];
+
+    protected $casts = [
+        'needed_on' => 'datetime',
     ];
 }
